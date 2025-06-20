3ca6afc288582b815af306d96b70eb50b1645096
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2025-06-20 15:27:09 +0530

    Implement Google Auth & Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { useUser } from "@clerk/nextjs";
+
+const Header = () => {
+  const { user } = useUser();
+
+  return (
+    <div className="flex items-center justify-between p-5">
+      {user && (
+        <h1 className="tex-2xl font-bold">
+          {user?.firstName}
+          {`'s`} Space
+        </h1>
+      )}
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
